635d438f71ceefbf4317d1a38bb08af05714d810
Kevin Koeks <kevin_koeks@hotmail.com>
2024-12-04 12:10:23 +0100

    form aangepast om alleen images te accepteren

--- Homework/product/insert-product.php
+++ Homework/product/insert-product.php
@@ -7,13 +7,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $prijsPerStuk = $_POST['prijsPerStuk'];
     $foto = $_FILES['foto']; //Pakt de geupload bestand en bewaard het in $foto
 
-    // Upload de foto in map uploads
+    // Upload de foto in map uploads (maak de map als je het niet hebt)
     $fotoPath = "../uploads/" . basename($foto['name']); // basename = veilig de originele bestand naam overnemen
-    $fotoSaved = move_uploaded_file($foto['tmp_name'], $fotoPath);
-
-
-
-
+    $fotoSaved = move_uploaded_file($foto['tmp_name'], $fotoPath); //Zorgt dat de bestand bij de juiste locatie komt 
 
     if ($fotoSaved) {
         // Voeg het product toe via de Product-class
@@ -43,16 +39,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <!-- enctype is nodig voor uploaden van bestanden/files -->
     <form action="insert-product.php" method="POST" enctype="multipart/form-data">
         <label for="productNaam">Productnaam:</label>
-        <input type="text" name="productNaam"required><br>
+        <input type="text" name="productNaam" required><br>
 
         <label for="omschrijving">Omschrijving:</label>
-        <input type="text" name="omschrijving"required><br>
+        <input type="text" name="omschrijving" required><br>
 
         <label for="prijsPerStuk">Prijs Per Stuk:</label>
         <input type="number" name="prijsPerStuk" required><br>
 
         <label for="foto">Foto:</label>
-        <input type="file" name="foto" ><br>
+        <!--accept="image/*" zorgt dat alleen een image geupload wordt -->
+        <input type="file" name="foto" accept="image/*"><br>
 
         <button type="submit">Product Toevoegen</button>
     </form>
